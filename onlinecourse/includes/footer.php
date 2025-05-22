e0274c0aa70cb754b9d78e4ae79c179e0175938d
Aman Kumar Thakur <160416669+ThakurAmanKumar@users.noreply.github.com>
2025-05-22 23:28:57 +0530

    Add files via upload

---
+++ onlinecourse/includes/footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                &copy; 2025 @ Online Course Registration | All Rights Reserved
+            </div>
+        </div>
+    </div>
+</footer>
